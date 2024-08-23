1f9228482c320893d477fe5d08d3ccd1caaf0b8e
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2024-08-23 18:10:55 +0800

    components

---
+++ apps/web/src/lib/components/Topbar.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { toggleMode } from "mode-watcher";
+</script>
+
+<div class="flex justify-center fixed ">
+    <button on:click={toggleMode}>Toggle Mode</button>
+</div>
--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
-    import "$lib/styles/app.scss";
+    import { ModeWatcher } from 'mode-watcher';
+    import '$lib/styles/app.pcss';
+    import '$lib/styles/global.scss';
+    import Topbar from '../lib/components/Topbar.svelte';
 </script>
 
+<ModeWatcher defaultMode="system"/>
+<Topbar/>
 <slot/>

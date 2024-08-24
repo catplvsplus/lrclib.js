5f8a9170a0b1d91e3f4ccd1fff603219525a73e4
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2024-08-24 11:26:59 +0800

    landing page

--- apps/web/src/lib/components/Topbar.svelte
+++ apps/web/src/lib/components/Topbar.svelte
@@ -7,23 +7,23 @@
     import { goto } from '$app/navigation';
     import { onMount } from 'svelte';
     import { parseSearchParams } from '../helpers/utils';
+    import { search } from '../stores/search';
 
-    let search: string = '';
     let searching: boolean = false;
 
     onMount(() => {
-        search = parseSearchParams(window.location.search).q || '';
+        $search = parseSearchParams(window.location.search).q || '';
     });
 </script>
 
-<header class="flex justify-center items-center w-full h-14 py-2 px-3 fixed bg-background/90 backdrop-blur-lg backdrop-saturate-200 border-b">
+<header class="flex justify-center items-center w-full h-14 py-2 px-3 fixed bg-background/90 backdrop-blur-lg backdrop-saturate-200 border-b z-50">
     <div class="flex justify-between items-center w-full max-w-6xl" class:searching>
         <h1 class="text-xl font-medium shrink-0 mr-4 title">
             <a href="/">
                 Lrclib<span class="text-primary font-bold">.js</span>
             </a>
         </h1>
-        <form class="w-full px-1" on:submit|preventDefault={() => goto(`/search?q=${encodeURIComponent(search)}`)}>
+        <form class="w-full px-1" on:submit|preventDefault={() => $search.trim() &&goto(`/search?q=${encodeURIComponent($search)}`)}>
             <Input
                 placeholder="Search"
                 type="search"
@@ -32,7 +32,7 @@
                 class="max-w-72 bg-foreground/20 dark:bg-background search"
                 on:focus={() => searching = true}
                 on:blur={() => searching = false}
-                bind:value={search}
+                bind:value={$search}
             />
         </form>
         <div class="flex gap-1 shrink-0 actions">
--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<div class="h-dvh">
+<script lang="ts">
+    import * as PageHeader from "$lib/components/ui/page-header/index.js";
+    import { onMount } from 'svelte';
+    import { parseSearchParams } from '../lib/helpers/utils';
+    import Search from 'lucide-svelte/icons/search';
+    import { search } from '../lib/stores/search';
+    import { goto } from '$app/navigation';
+
+    onMount(() => {
+        $search = parseSearchParams(window.location.search).q || '';
+    });
+</script>
+
+<div class="container relative">
+    <PageHeader.Root class="pb-8">
+		<PageHeader.Heading>Lrclib<span class="text-primary">.js</span></PageHeader.Heading>
+		<PageHeader.Description class="text-muted-foreground">
+            A javascript library for interacting with <a href="http://lrclib.net" target="_blank" rel="noopener noreferrer" class="text-primary">lrclib.net</a> API
+		</PageHeader.Description>
+        <div class="flex justify-center items-center w-full space-x-4 py-4 md:pb-10">
+            <form
+                class="flex w-full max-w-80 h-11 border border-input bg-foreground/20 dark:bg-background ring-offset-background focus-within:ring-ring rounded-md overflow-hidden"
+                on:submit|preventDefault={() => $search.trim() && goto(`/search?q=${encodeURIComponent($search)}`)}
+            >
+                <input bind:value={$search} placeholder="Search" type="search" class="w-full max-w-72 bg-transparent px-3 py-2 text-sm outline-none placeholder:text-muted-foreground"/>
+                <button type="submit" class="shrink-0 w-10 h-10 flex justify-center items-center outline-none">
+                    <Search size="16" class="text-primary"/>
+                </button>
+            </form>
+        </div>
+	</PageHeader.Root>
 </div>
+
+<style lang="scss">
+    .container {
+        @apply pt-14;
+    }
+</style>

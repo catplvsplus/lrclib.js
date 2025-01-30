9f81ab36545ca1a5171451ffead719e0314f1013
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2025-01-30 22:53:51 +0800

    pwa

--- apps/website/src/routes/+layout.svelte
+++ apps/website/src/routes/+layout.svelte
@@ -1,7 +1,38 @@
 <script lang="ts">
-	import '$lib/styles/app.pcss'
-;
+	import '$lib/styles/app.pcss';
+    import { onMount } from 'svelte';
+    import { Toaster } from '../lib/components/ui/sonner';
+    import { toast } from 'svelte-sonner';
+    import { ModeWatcher, toggleMode } from 'mode-watcher';
+
 	let { children } = $props();
+
+    async function updateServiceWorker() {
+        const registration = await navigator.serviceWorker.ready;
+
+        registration.addEventListener('updatefound', () => {
+            const newWorker = registration.installing;
+
+            newWorker?.addEventListener('statechange', () => {
+                if (newWorker.state === 'installed') {
+                    toast.info('New update is available!', {
+                        duration: 60000,
+                        action: {
+                            label: 'Reload',
+                            onClick: () => {
+                                newWorker.postMessage({ type: 'SKIP_WAITING' });
+                                window.location.reload();
+                            }
+                        }
+                    });
+                }
+            });
+        });
+    }
+
+    onMount(() => updateServiceWorker());
 </script>
 
+<Toaster/>
+<ModeWatcher defaultMode="system"/>
 {@render children()}
---
+++ apps/website/src/service-worker/index.ts
@@ -0,0 +1,65 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference lib="webworker" />
+
+declare let self: ServiceWorkerGlobalScope;
+
+import { build, files, version } from '$service-worker';
+
+const cacheName = `cache-v${version}`;
+const assets = [...files, ...build];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(addFilesToCache());
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clearOldCache());
+});
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.method !== 'GET') return;
+
+    event.respondWith(serveFromCache(event.request));
+});
+
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
+});
+
+async function addFilesToCache(files?: RequestInfo[]) {
+    const cache = await caches.open(cacheName);
+    await cache.addAll([...assets, ...files || []]);
+}
+
+async function clearOldCache() {
+    const keys = await caches.keys();
+
+    for (const key of keys) {
+        if (key !== cacheName) await caches.delete(key);
+    }
+}
+
+async function serveFromCache(request: Request) {
+    const url = new URL(request.url);
+    const cache = await caches.open(cacheName);
+
+    // if (assets.includes(url.pathname)) {
+    //     const cached = await cache.match(request);
+    //     if (cached) return cached;
+    // }
+
+    try {
+        const response = await fetch(request);
+
+        if (response.ok && url.protocol.startsWith('http')) {
+            cache.put(request, response.clone());
+        }
+
+        return response;
+    } catch (error) {
+        const cached = await cache.match(request);
+        if (cached) return cached;
+    }
+
+    return new Response(null, { status: 404 });
+}

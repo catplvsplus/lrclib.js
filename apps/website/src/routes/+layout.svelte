8900bc4f4ef4bc29b8be50321078d2a89997ef76
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2025-08-02 14:40:02 +0800

    setup

--- apps/website/src/routes/+layout.svelte
+++ apps/website/src/routes/+layout.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
 	import '$lib/styles/app.css';
+    import { ModeWatcher } from 'mode-watcher';
+    import { Toaster } from '$lib/components/ui/sonner/index';
 
 	let { children } = $props();
 </script>
 
+<ModeWatcher/>
+<Toaster/>
+
 <svelte:head>
+    <title>Lrclib.js</title>
 </svelte:head>
 
 {@render children?.()}

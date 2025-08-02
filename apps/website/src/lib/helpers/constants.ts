08dd041709f3d4a37118c7d8934581406a93dfa0
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2025-08-03 01:52:42 +0800

    home

--- apps/website/src/lib/helpers/constants.ts
+++ apps/website/src/lib/helpers/constants.ts
@@ -1,4 +1,5 @@
 export const links = {
     github: 'https://github.com/catplvsplus/lrclib.js',
-    contact: 'https://catplvsplus.is-a.dev'
+    contact: 'https://catplvsplus.is-a.dev',
+    lrclib: 'https://lrclib.net'
 };

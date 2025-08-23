cb7b2e75e21380ea55269e7d14fbb830508f3cf3
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2025-08-23 22:55:33 +0800

    Refactor player components: remove unused imports and add PlayerControls for improved functionality

--- apps/website/src/routes/(main)/player/+page.svelte
+++ apps/website/src/routes/(main)/player/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-    import { player } from '$lib/helpers/classes/Player.svelte';
     import { onMount } from 'svelte';
     import { userInterface } from '$lib/helpers/classes/UserInterface.svelte';
-    import { resolve } from '$app/paths';
     import QueueCard from '$lib/components/shared/queue/QueueCard.svelte';
+    import PlayerControls from '$lib/components/shared/player/PlayerControls.svelte';
 
     onMount(() => {
         userInterface.playerMode = 'hidden';
@@ -12,12 +11,11 @@
             userInterface.playerMode = 'visible';
         };
     });
-
-    let coverURL = $derived(player.playing?.coverImageURL ?? `${resolve('/')}cover.png`);
 </script>
 
 <div class="grid lg:grid-cols-2 xl:grid-cols-3 gap-2 w-full">
     <div class="grid gap-2 xl:col-span-2">
+        <PlayerControls/>
     </div>
     <QueueCard/>
 </div>

148114cd0dc519d2d2fd7f098005d32ed00f3637
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2025-11-16 12:29:36 +0800

    refactor: remove prerender and ssr exports from publish page

--- apps/website/src/routes/(main)/publish/+page.ts
+++ apps/website/src/routes/(main)/publish/+page.ts
@@ -3,9 +3,6 @@ import { zod } from 'sveltekit-superforms/adapters';
 import { publishTrackSchema } from '$lib/helpers/schema';
 import { publishDraft } from '$lib/helpers/classes/PublishDraft.svelte';
 
-export const prerender = true;
-export const ssr = false;
-
 export async function load() {
     return {
         form: await superValidate({

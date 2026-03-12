6dc89566389eb717210d804c67c2bf199b88d846
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2026-03-12 21:16:29 +0800

    fix

--- apps/website/src/routes/(home)/+page.ts
+++ apps/website/src/routes/(home)/+page.ts
@@ -1,7 +1,7 @@
 import { codeToHtml } from 'shiki';
 
 export async function load({ url, fetch }) {
-    const code = await (await fetch(`${url.origin}/usage/index.ts`)).text().catch(() => null);
+    const code = await fetch(`${url.origin}/usage/index.ts`).then(res => res.text()).catch(() => null);
 
     return {
         code: code

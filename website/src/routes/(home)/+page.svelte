9626c01a6a7583be258e624cdd68252b43eb9618
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2026-09-26 01:18:00 +0800

    feat: implement search autocomplete and OpenSearch support for enhanced user experience

--- website/src/routes/(home)/+page.svelte
+++ website/src/routes/(home)/+page.svelte
@@ -1,11 +1,40 @@
 <script lang="ts">
     import { InputGroup, InputGroupButton, InputGroupInput } from '$lib/components/ui/input-group';
-    import { SearchIcon } from '@lucide/svelte';
+    import { ListSortDescendingIcon, LoaderCircleIcon, SearchIcon, TextCursorInputIcon } from '@lucide/svelte';
     import { AUTHOR_URL } from '$lib/helpers/constants';
     import { goto } from '$app/navigation';
     import { resolve } from '$app/paths';
+    import { resource } from 'runed';
+    import lrclib from 'lrclib.js';
+    import { Button } from '../../lib/components/ui/button';
+    import { fly } from 'svelte/transition';
 
     let search = $state('');
+    let isFocused = $state(false);
+    let autocompleteIndex = $state(-1);
+    let autocompleteContainer: HTMLDivElement|null = $state(null);
+    let results = resource(
+        () => search,
+        async (search) => {
+            if (!search.trim()) return [];
+
+            const results = lrclib
+                .search(search.trim())
+                .then(r => r.splice(0, 5))
+                .catch(() => []);
+
+            autocompleteIndex = -1;
+
+            return results;
+        },
+        {
+            debounce: 100,
+            throttle: 500,
+            lazy: true
+        }
+    );
+
+    let focusTimeout: NodeJS.Timeout|null = null;
 
     function onSearch() {
         goto(`${resolve('/(app)/search')}?q=${encodeURIComponent(search.trim())}`);
@@ -24,13 +53,47 @@
             </p>
         </section>
         <section class="w-full p-4 max-w-lg min-h-48">
-            <InputGroup class="rounded-xl ring-primary/20! focus-within:border-primary! hover:border-primary! hover:shadow-lg focus-within:shadow-lg shadow-primary/20 duration-300 transition-all">
+            <InputGroup class="relative rounded-xl ring-primary/20! focus-within:border-primary! hover:border-primary! hover:shadow-lg focus-within:shadow-lg shadow-primary/20 duration-300 transition-all">
                 <InputGroupInput
                     placeholder="Search for a song, artist, or album"
                     type="text"
                     class="placeholder:text-sm px-3"
                     bind:value={search}
-                    onkeyup={e => e.key == 'Enter' && onSearch()}
+                    onfocus={() => {
+                        if (focusTimeout) clearTimeout(focusTimeout);
+                        isFocused = true;
+                    }}
+                    onblur={() => {
+                        if (focusTimeout) clearTimeout(focusTimeout);
+                        focusTimeout = setTimeout(() => isFocused = false, 100);
+                    }}
+                    onkeydown={e => {
+                        switch (e.key) {
+                            case 'Enter':
+                                if (autocompleteIndex >= 0 && results.current?.length) {
+                                    const selectedResult = results.current[autocompleteIndex];
+
+                                    if (selectedResult) {
+                                        goto(resolve('/(app)/track/[id]', { id: String(selectedResult.id) }));
+                                    }
+                                } else {
+                                    onSearch();
+                                }
+                                break;
+                            case 'ArrowDown':
+                                if (results.current?.length) {
+                                    e.preventDefault();
+                                    autocompleteIndex = (autocompleteIndex + 1) % results.current.length;
+                                }
+                                break;
+                            case 'ArrowUp':
+                                if (results.current?.length) {
+                                    e.preventDefault();
+                                    autocompleteIndex = (autocompleteIndex - 1 + results.current.length) % results.current.length;
+                                }
+                                break;
+                        }
+                    }}
                 />
                 <InputGroupButton
                     size="icon-sm"
@@ -39,6 +102,44 @@
                 >
                     <SearchIcon/>
                 </InputGroupButton>
+                {#if isFocused}
+                    <div
+                        bind:this={autocompleteContainer}
+                        transition:fly={{ y: 5, duration: 150 }}
+                        class="absolute top-full left-0 grid border rounded-xl bg-background mt-1 w-full max-h-48 overflow-y-auto shadow z-10"
+                    >
+                        {#if results.loading}
+                            <p class="text-xs text-muted-foreground px-3 py-2 flex items-center gap-1">
+                                <LoaderCircleIcon class="animate-spin size-3.5"/>
+                                Loading...
+                            </p>
+                        {:else if !results.current?.length && search.trim()}
+                            <p class="text-xs text-muted-foreground px-3 py-2 flex items-center gap-1">
+                                <ListSortDescendingIcon class="size-3.5"/>
+                                No results found.
+                            </p>
+                        {:else if !results.current?.length}
+                            <p class="text-xs text-muted-foreground px-3 py-2 flex items-center gap-1">
+                                <TextCursorInputIcon class="size-3.5"/>
+                                Start typing to search.
+                            </p>
+                        {:else}
+                            {#each results.current as result, index (result.id)}
+                                {@const isSelected = index === autocompleteIndex}
+                                <Button 
+                                    variant="ghost"
+                                    class={[
+                                        "block text-sm w-full line-clamp-2 whitespace-normal text-start h-fit py-1 rounded-xl",
+                                        isSelected ? 'bg-primary/10 text-primary autocomplete-item' : 'text-foreground',
+                                    ]}
+                                    href={resolve('/(app)/track/[id]', { id: String(result.id) })}
+                                >
+                                    <span class="text-primary">{result.trackName}</span> <span class="text-xs">by {result.artistName}</span>
+                                </Button>
+                            {/each}
+                        {/if}
+                    </div>
+                {/if}
             </InputGroup>
         </section>
     </main>
